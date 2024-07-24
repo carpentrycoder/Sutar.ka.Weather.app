afd35c59840028288d4af711b1d769feeb85f5c7
Sutar <nikhilsutar621@gmail.com>
2024-07-24 20:49:12 +0530

    Basic Update in Js

--- script.js
+++ script.js
@@ -36,27 +36,27 @@ search.addEventListener('click', () => {
 
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = 'images/clear.png';
+                    image.src = 'clear.png';
                     break;
 
                 case 'Rain':
-                    image.src = 'images/rain.png';
+                    image.src = 'rain.png';
                     break;
 
                 case 'Snow':
-                    image.src = 'images/snow.png';
+                    image.src = 'snow.png';
                     break;
 
                 case 'Clouds':
-                    image.src = 'images/cloud.png';
+                    image.src = 'cloud.png';
                     break;
 
                 case 'Haze':
-                    image.src = 'images/mist.png';
+                    image.src = 'mist.png';
                     break;
                 
                 case 'Drizzle':
-                    image.src = 'images/drizzle.png'
+                    image.src = 'drizzle.png'
                     break;
                     
                 default:
